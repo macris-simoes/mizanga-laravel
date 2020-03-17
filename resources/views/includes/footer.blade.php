a57b81f8daddbd509dc7fdc25c8a86935c43782f
kabottino <kabottino@hotmail.com>
2020-03-17 10:10:37 -0300

    add view, route, controller e css

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="p-0 mt-2 bg-primary text-white text-center">
+        <p class="mb-0">Mizanga 2020</p>
+    </div>
+    </footer>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,8 +13,69 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
 
-    //cris teste :)
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/inscricao-usuario', function () {
+    return view('inscricao-usuario');
+});
+Route::get('/contato', function () {
+    return view('contato');
+});
+//------------INSCRITOS------------
+Route::get('/inscrito-enviar-trabalho', function () {
+    return view('inscrito-enviar-trabalho');
+});
+Route::get('/inscrito-home', function () {
+    return view('inscrito-home');
+});
+Route::get('/inscrito-trabalhos-cadastrados', function () {
+    return view('inscrito-trabalhos-cadastrados');
+});
+Route::get('/instrucoes-inscricao', function () {
+    return view('instrucoes-inscricao');
+});
+//-----------LINKS------------
+Route::get('/criterios-trabalho', function () {
+    return view('link-criterios-trabalho');
+});
+Route::get('/instrucao-avaliadores', function () {
+    return view('link-instrucao-avaliadores');
+});
+//----------LOGIN--------------
+Route::get('/login', function () {
+    return view('login');
+});
+//----------ADMIN-------------
+Route::get('/admin-config-inscrito', function () {
+    return view('admin-config-inscrito');
+});
+
+Route::get('/admin-config-parecerista', function () {
+    return view('admin-config-parecerista');
+});
+Route::get('/admin-config-trabalhos', function () {
+    return view('admin-config-trabalhos');
+});
+
+Route::get('/gerenciar-parecerista', function () {
+    return view('admin-gerenciar-parecerista');
+});
+Route::get('/admin-home', function () {
+    return view('admin-home');
+});
+Route::get('/substituir-parecerista', function () {
+    return view('admin-substituir-parecerista');
+});
+//--------Parecerista----------
+Route::get('/parecerista-home', function () {
+    return view('parecerista-home');
+});
+Route::get('/parecerista-vizualizar-trabalho', function () {
+    return view('parecerista-vizualizar-trabalho');
+});
+Route::get('/avaliar-trabalho', function () {
+    return view('parecerista-avaliar-trabalho');
 });
