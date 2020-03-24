6405f0c3863893375a8149a71a966ed45c551f55
Stephanie Tavares <stephanie.diast@hotmail.com>
2020-03-23 22:51:59 -0300

    add names

--- resources/views/admin-config-parecerista.blade.php
+++ resources/views/admin-config-parecerista.blade.php
@@ -11,19 +11,20 @@
         </div>
 
         <form action="" method="post">
+            @csrf
             <div class="my-3">
                 <div class="form-row">
                     <div class="form-group col-md-4">
                         <label for="cpf">CPF*</label>
-                        <input type="text" class="form-control" id="cpf" placeholder="cpf">
+                        <input type="text" class="form-control" id="cpf" placeholder="CPF" name="cpf">
                     </div>
                     <div class="form-group col-md-4">
                         <label for="nome">Nome*</label>
-                        <input type="text" class="form-control" id="nome" placeholder="Nome">
+                        <input type="text" class="form-control" id="nome" placeholder="Nome" name="name">
                     </div>
                     <div class="form-group col-md-4">
                         <label for="email">E-mail*</label>
-                        <input type="text" class="form-control" id="email" placeholder="E-mail">
+                        <input type="text" class="form-control" id="email" placeholder="E-mail" name="email">
                     </div>
                 </div>
             </div>
@@ -35,11 +36,11 @@
                         Eixo 01 - Lorem ipsum dolor sit amet consectetur
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions1" id="eixoUmSim" value="sim">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions1" id="eixoUmSim" value="sim" name="axis_01_yes">
                         <label class="form-check-label" for="eixoUmSim">Sim</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions1" id="eixoUmNao" value="nao">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions1" id="eixoUmNao" value="nao" name="axis_01_no">
                         <label class="form-check-label" for="eixoUmNao">Não</label>
                     </div>
                 </div>
@@ -48,11 +49,11 @@
                         Eixo 02 - Lorem ipsum dolor sit amet consectetur
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="eixoDoisSim" value="sim">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="eixoDoisSim" value="sim" name="axis_02_yes">
                         <label class="form-check-label" for="eixoDoisSim">Sim</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="eixoDoisNao" value="nao">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="eixoDoisNao" value="nao" name="axis_02_no">
                         <label class="form-check-label" for="eixoDoisNao">Não</label>
                     </div>
                 </div>
@@ -61,11 +62,11 @@
                         Eixo 03 - Lorem ipsum dolor sit amet consectetur
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions3" id="eixoTresSim" value="sim">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions3" id="eixoTresSim" value="sim" name="axis_03_yes">
                         <label class="form-check-label" for="eixoTresSim">Sim</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions3" id="eixoTresNao" value="nao">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions3" id="eixoTresNao" value="nao" name="axis_03_no">
                         <label class="form-check-label" for="eixoTresNao">Não</label>
                     </div>
                 </div>
@@ -74,11 +75,11 @@
                         Eixo 04 - Lorem ipsum dolor sit amet consectetur
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="eixoQuatroSim" value="sim">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="eixoQuatroSim" value="sim" name="axis_04_yes">
                         <label class="form-check-label" for="eixoQuatroSim">Sim</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="eixoQuatroNao" value="nao">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="eixoQuatroNao" value="nao" name="axis_04_no">
                         <label class="form-check-label" for="eixoQuatroNao">Não</label>
                     </div>
                 </div>
@@ -87,7 +88,7 @@
             <div class="my-3">
                 <h5>Após salvar:</h5>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="exampleRadios" id="opcao1" value="option1">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="opcao1" value="option1" name="accept_letter">
                     <label class="form-check-label" for="opcao1">
                         Enviar carta convite
                     </label>
