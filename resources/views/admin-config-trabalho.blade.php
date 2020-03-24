e51edba63a26d71441abb72bbac741a7e60cbe32
Stephanie Tavares <stephanie.diast@hotmail.com>
2020-03-23 23:51:20 -0300

    add names

--- resources/views/admin-config-trabalho.blade.php
+++ resources/views/admin-config-trabalho.blade.php
@@ -31,28 +31,28 @@
         
       <!-- início do form de modalidades -->
         <form>
-        
+        @csrf
         <!-- início da div form-row -->
           <div class="form-row">
 
             <div class="font-weight-bold form-group col-md-12">
               <label for="validationTooltip01">Identificação</label>
-              <input type="text" class="form-control" id="validationTooltip01" placeholder="Escolha um nome" required>
+              <input type="text" class="form-control" id="validationTooltip01" placeholder="Escolha um nome" name="abstract_id" required>
             </div>
           
             <div class="form-group col-md-5">
               <label for="validationTooltip04">Início das inscrições</label>
-              <input type="datetime-local" class="form-control" id="validationTooltip04" placeholder="dd/mm/aaaa" required>
+              <input type="datetime-local" class="form-control" id="validationTooltip04" placeholder="dd/mm/aaaa" name="start_date" required>
             </div>
 
             <div class="form-group col-md-5">
               <label for="validationTooltip05">Fim das inscrições</label>
-              <input type="datetime-local" class="form-control" id="validationTooltip05" placeholder="dd/mm/aaaa" required>
+              <input type="datetime-local" class="form-control" id="validationTooltip05" placeholder="dd/mm/aaaa" name="end_date" required>
             </div>
 
             <div for="validationTooltip06" class="form-group col-md-2" data-toggle="tooltip" data-placement="right" title="Sem limite? Deixe em branco">
               <label>Coautores</label>
-              <input type="number" class="form-control" id="validationTooltip06">
+              <input type="number" class="form-control" id="validationTooltip06" name="coauthor">
             </div>
 
           </div>
@@ -68,12 +68,12 @@
               <h6 class="col-md-12">Configurar título</h6>
               <div class="col-md-6">
                 <label for="exampleFormControlTextarea1">Mínimo de caracteres</label>
-                <input class="form-control" type="number" id="exampleFormControlTextarea1" placeholder="inserir número">
+                <input class="form-control" type="number" id="exampleFormControlTextarea1" placeholder="inserir número" name="title_min_char">
               </div>
 
               <div class="col-md-6">
                 <label for="exampleFormControlTextarea2">Máximo de caracteres</label>
-                <input class="form-control" type="number" id="exampleFormControlTextarea2" placeholder="inserir número">
+                <input class="form-control" type="number" id="exampleFormControlTextarea2" placeholder="inserir número" name="title_max_char">
               </div>
             </div>
 
@@ -83,12 +83,12 @@
               <h6 class="col-md-12">Configurar resumo</h6>
               <div class="col-md-6">
                 <label for="exampleFormControlTextarea1">Mínimo de caracteres</label>
-                <input class="form-control" type="number" id="exampleFormControlTextarea1" placeholder="inserir número">
+                <input class="form-control" type="number" id="exampleFormControlTextarea1" placeholder="inserir número" name="resume_min_char">
               </div>
 
               <div div class="col-md-6">
                 <label for="exampleFormControlTextarea2">Máximo de caracteres</label>
-                <input class="form-control" type="number" id="exampleFormControlTextarea2" placeholder="inserir número">
+                <input class="form-control" type="number" id="exampleFormControlTextarea2" placeholder="inserir número" name="resume_max_char">
               </div>
           
             </div>
@@ -98,7 +98,7 @@
           <div class="form-row">
             <div class="col-md-12">
               <label for="exampleFormControlTextarea1">Texto de instrução aos autores</label>
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="instructions_coauth"></textarea>
             </div>
 
             <div class="col-md-12">
