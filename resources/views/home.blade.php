cfc8fb39f3eec01e605c68d0698a5807f285c889
carolfps <37373110+carolfps@users.noreply.github.com>
2020-03-25 18:20:19 -0300

    inserindo contagem regressiva em JavaScript

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,9 @@
       justify-content: center;
       width: 100%;
       height: 50%;
+      font-size: 3rem;
+      font-weight: bold;
+      font-family: 'Roboto';
     }
   </style>
   <div class="container-fluid" id="topo">
@@ -40,8 +43,8 @@
 
     <div class="border border-primary my-4">
       <!-- Contador -->
-      <div class="contador my-4">
-        <iframe src="http://free.timeanddate.com/countdown/i73ylhfn/n233/cf12/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pcfff/tcfff/fs100/szw500/szh210/tatTEMPO%20PARA%20O%20FINAL%20DAS%20INSCRI%C3%87%C3%95ES/tac000/tpc000/mac000/mpc000/iso2020-11-16T00:00:00" allowTransparency="true" frameborder="0" width="553" height="105"></iframe>
+      <div class="contador my-4" id="contador">
+        
       </div>
 
       <!--botão inscreva-se -->
@@ -104,6 +107,39 @@
     @include('includes.footer')
     @include('includes.script')
   </div>
+
+  <script>
+    // Definino a data para o countdown
+    var dataDaRegressiva = new Date("Apr 29, 2020 23:59:59").getTime();
+
+    function executaRegressiva() {
+      // Pega a data e a hora do dia de hoje
+      var agora = new Date().getTime();
+        
+      // Encontra a diferença entre a data de hoje e a data do countdown
+      var diferenca = dataDaRegressiva - agora;
+        
+      // Transformando a diferença em dias, horas, minutos e segundos 
+      var dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+      var horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
+      var segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
+        
+      // Inserindo o resultado (dias, horas, minutos e segundos) no elemento (id="contador")
+      document.getElementById("contador").innerHTML = dias + "d " + horas + "h "
+      + minutos + "m " + segundos + "s ";
+        
+      // Quando o tempo de inscrições acaba, aparece "Inscrições encerradas" no elemento
+      if (diferenca < 0) {
+        clearInterval(x);
+        document.getElementById("contador").innerHTML = "Inscrições Encerradas!";
+      }
+    }
+    executaRegressiva();
+    // Atualiza a contagem regressiva a cada 1 segundo
+    var x = setInterval(executaRegressiva, 1000);
+  </script>
+
 </body>
 
 </html>
