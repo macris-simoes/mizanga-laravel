88dec87a2ed773df58578bdd522b8efddb41e864
carolfps <37373110+carolfps@users.noreply.github.com>
2020-04-05 15:50:15 -0300

    criando migrations e model da tabela registrations

---
+++ database/migrations/2020_04_05_165233_create_registrations_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateRegistrationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registrations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('register_modality');
+            $table->string('file')->nullable();
+            $table->char('cpf',11)->unique();
+            $table->string('rg',14)->unique();
+            $table->string('emissor');
+            $table->string('name');
+            $table->date('birth_date');
+            $table->string('social_name');
+            $table->string('institution')->nullable();
+            $table->string('education_level');
+            $table->string('grad_school');
+            $table->string('profession');
+            $table->string('filiation')->nullable();
+            $table->string('job_title')->nullable();
+            $table->string('expertise')->nullable();
+            $table->string('location');
+            $table->string('city');
+            $table->string('uf');
+            $table->string('country')->nullable();
+            $table->string('home_phone',11)->nullable();
+            $table->string('work_phone',11)->nullable();
+            $table->string('mobile_phone',11)->nullable();
+            $table->string('email')->unique();
+            $table->boolean('accessibility');
+            $table->text('accessibility_info')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('color');
+            $table->boolean('use_social_name');
+        });
+        
+        $prefix = DB::getTablePrefix();
+        DB::update("ALTER TABLE ".$prefix."registrations AUTO_INCREMENT = 15000;");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registrations');
+    }
+}
