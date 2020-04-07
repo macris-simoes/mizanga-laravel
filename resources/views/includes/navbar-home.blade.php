139beaf2cdee0534eafa372da4e236c01bb5e3a6
carolfps <37373110+carolfps@users.noreply.github.com>
2020-04-07 16:50:04 -0300

    ajustes provisorios na navbar

--- resources/views/includes/navbar-home.blade.php
+++ resources/views/includes/navbar-home.blade.php
@@ -1,13 +1,13 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-white container">
+<nav class="navbar navbar-expand-lg navbar-light bg-white container ">
 
-      <a class="navbar-brand" href="/home"> 
+      <a class="navbar-brand mr-auto" href="/home"> 
         <h2 class="text-primary"> Mizanga</h2> 
       </a>
+      <a class="btn btn-primary rounded-right d-lg-none d-xl-none mr-1" href="/login" style="font-family: Segoe UI, Roboto;">LOGIN</a> 
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse " id="navbarNav">
-        <!-- início navbar antes do login -->
+      <div class="collapse navbar-collapse ml-3" id="navbarNav">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
             <a class="nav-link" href="/home">Início</a>
@@ -15,16 +15,12 @@
           <li class="nav-item">
             <a class="nav-link" href="/instrucoes-inscricao"> Faça sua inscrição</a>
           </li>
-        <!-- fim navbar antes do login -->
           <li class="nav-item">
             <a class="nav-link" href="/contato">Contato</a>
           </li>
           
         </ul>
-    <!-- BUTTON Não está funcionando :(  -->
-        <a class="btn btn-primary nav-link rounded-right" href="/login">LOGIN</a> 
-        
+        <a class="btn btn-primary nav-link rounded-right d-none d-lg-block d-xl-block" href="/login">LOGIN</a> 
+      </div>
       
-        <!-- início dropdown do perfil do usuário *** STE, NÃO ESTÁ RESPONSIVO, NÃO É UM PROBLEMA MAS SÓ AVISANDO-->
-        
     </nav>
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,60 +1,68 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white container">
-    <!-- 26/12 Cris não conseguiu fazer fixed-top direito -->
-    <a class="navbar-brand" href="/home">
-      <h2 class="text-primary"> Mizanga</h2>
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+  <a class="navbar-brand mr-auto" href="/home">
+    <h2 class="text-primary"> Mizanga</h2>
+  </a>
+  <a class="btn btn-primary rounded-right d-lg-none d-xl-none mr-1" href="/login"  style="font-family: Segoe UI, Roboto;">LOGIN</a> 
+  <div class="dropdown d-lg-none d-xl-none mr-1">
+    <!-- parte de fora -->
+    <button type="button" class="btn dropdown-toggle" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20">
+      <img src="https://www.searchpng.com/wp-content/uploads/2019/02/Deafult-Profile-Pitcher.png" width="40px" height="40px">
     </button>
-    <div class="collapse navbar-collapse " id="navbarNav">
-      <!-- início navbar antes do login -->
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="/home">Início</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/instrucoes-inscricao"> Faça sua inscrição</a>
-        </li>
-        <!-- fim navbar antes do login -->
-        <!-- INÍCIO navbar de ADMIN -->
-        <li class="nav-item">
-          <a class="nav-link" href="/admin"> Área do admin </a>
-        </li>
-        <!-- fim navbar de ADMIN -->
-        <!-- início navbar INSCRITO -->
-        <li class="nav-item">
-          <a class="nav-link" href="/inscrito"> Área do inscrito </a>
-        </li>
-        <!-- FIM navbar INSCRITO -->
-        <!-- início navbar PARECERISTA -->
-        <li class="nav-item">
-          <a class="nav-link" href="/parecerista"> Área do parecerista </a>
-        </li>
-        <!-- FIM navbar PARECERISTA -->
-        <li class="nav-item">
-          <a class="nav-link" href="/contato">Contato</a>
-        </li>
-      </ul>
-      <!-- BUTTON Não está funcionando :(  -->
-      <a class="btn btn-primary nav-link rounded-right" href="/login">LOGIN</a>
-      <!-- início dropdown do perfil do usuário *** STE, NÃO ESTÁ RESPONSIVO, NÃO É UM PROBLEMA MAS SÓ AVISANDO-->
-      <div class="dropdown">
-        <!-- parte de fora -->
-        <button type="button" class="btn dropdown-toggle" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20">
-          <img src="https://www.searchpng.com/wp-content/uploads/2019/02/Deafult-Profile-Pitcher.png" width="40px" height="40px">
-        </button>
-        <!-- parte de dentro (dropdown) -->
-        <div class="dropdown-menu  dropdown-menu-right" class="card" aria-labelledby="dropdownMenuOffset">
-          <div>
-            <img src="https://www.searchpng.com/wp-content/uploads/2019/02/Deafult-Profile-Pitcher.png" width="70px" height="70px" class="card-img-center" alt="...">
-            <div class="card-body ">
-              <h5 class="card-title">Seus Dados</h5>
-              <p class="card-text">Nome Sobrenome</p>
-            </div>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="/inscrito">Editar dados</a>
-          </div>
+    <!-- parte de dentro (dropdown) -->
+    <div class="dropdown-menu  dropdown-menu-right" class="card" aria-labelledby="dropdownMenuOffset">
+      <div>
+        <img src="https://www.searchpng.com/wp-content/uploads/2019/02/Deafult-Profile-Pitcher.png" width="70px" height="70px" class="card-img-center" alt="...">
+        <div class="card-body ">
+          <h5 class="card-title">Seus Dados</h5>
+          <p class="card-text">Nome Sobrenome</p>
         </div>
-        <!-- fim dropdown do perfil do usuário -->
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="/inscrito">Editar dados</a>
       </div>
+    </div>
+  </div>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse ml-3" id="navbarNav">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="/home">Início</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/instrucoes-inscricao"> Faça sua inscrição</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/admin"> Área do admin </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/inscrito"> Área do inscrito </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/parecerista"> Área do parecerista </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/contato">Contato</a>
+      </li>
+    </ul>
+    <a class="btn btn-primary nav-link rounded-right d-none d-lg-block d-xl-block" href="/login">LOGIN</a> 
+  </div>
+  <div class="dropdown d-none d-lg-block d-xl-block">
+    <!-- parte de fora -->
+    <button type="button" class="btn dropdown-toggle" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20">
+      <img src="https://www.searchpng.com/wp-content/uploads/2019/02/Deafult-Profile-Pitcher.png" width="40px" height="40px">
+    </button>
+    <!-- parte de dentro (dropdown) -->
+    <div class="dropdown-menu  dropdown-menu-right" class="card" aria-labelledby="dropdownMenuOffset">
+      <div>
+        <img src="https://www.searchpng.com/wp-content/uploads/2019/02/Deafult-Profile-Pitcher.png" width="70px" height="70px" class="card-img-center" alt="...">
+        <div class="card-body ">
+          <h5 class="card-title">Seus Dados</h5>
+          <p class="card-text">Nome Sobrenome</p>
+        </div>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="/inscrito">Editar dados</a>
+      </div>
+    </div>
+  </div>
   </nav>
