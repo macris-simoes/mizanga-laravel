667f43f5e10b3c999f7e433e7fbd12a8c178ebae
macris-simoes <macris.simoes@gmail.com>
2020-04-09 18:01:16 -0300

    added template's footer

--- resources/views/includes/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <div class="p-0 mt-4 bg-primary text-white text-center">
-        <p class="mb-0">Mizanga 2020</p>
-    </div>
-    </footer>
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="./css/style.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Amaranth&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="icon" href="{{ URL::asset('/img/favicon2.png') }}" type="image/x-icon"/>
+    <link rel="icon" href="{{ URL::asset('/img/favicon2.png') }}" type="image/x-icon" />
     <style>
         body {
             margin: 0;
@@ -25,9 +26,17 @@
             flex: 1 0 auto;
         }
     </style>
-    <title>Mizanga</title>
+    <title>Mizanga Beta</title>
 </head>
+
 <body>
-    
+    @yield('content')
+
+    <footer>
+        <div class="p-0 mt-4 bg-primary text-white text-center">
+            <p class="mb-0">Mizanga 2020</p>
+        </div>
+    </footer>
 </body>
+
 </html>
